924cb78c8137ea3c4a16d7a3da5bf93031129844
Adarsh Prashar <adarsh.prashar32@gmail.com>
2021-12-12 18:38:08 +0530

    Initialize project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import './App.css';
+import Product from './pages/Product/Product'
+import DetailPage from './components/DetailPage/DetailPage'
+import {Header} from './components/Header/Header'
+import 'font-awesome/css/font-awesome.css'
+import {ProductProvider} from "./Context";
+
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+			<Header />
+			<BrowserRouter>
+				<Switch>
+					<Route exact path="/" component={Product} />
+					<Route path="/details" component={DetailPage} /> 
+				</Switch>
+			</BrowserRouter>
+		</React.Fragment>
   );
 }
 
